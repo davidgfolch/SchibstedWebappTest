0fcee0b4ff3fe99bb81522e8ffe3914b8fdcc622
DavidGFolch <davidgfolch@gmail.com>
2017-02-09 16:41:45 +0100

    RxJava

--- src/main/java/com/schibsted/webapp/server/Server.java
+++ src/main/java/com/schibsted/webapp/server/Server.java
@@ -5,11 +5,16 @@ import static com.schibsted.webapp.di.DIFactory.inject;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
+import com.schibsted.webapp.server.handler.HandlerFactory;
+import com.schibsted.webapp.server.handler.MVCHandler;
+import com.schibsted.webapp.server.handler.WebHandler;
 import com.schibsted.webapp.server.helper.ReflectionHelper;
 import com.schibsted.webapp.server.helper.ServerConfigHelper;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+
+import rx.Observable;
 
 @SuppressWarnings("restriction")
 public class Server implements ILogger {
@@ -20,20 +25,38 @@ public class Server implements ILogger {
 	private final Config config = inject(Config.class);
 	private final ServerConfigHelper serverConfigHelper = inject(ServerConfigHelper.class);
 	private final ReflectionHelper reflectionHelper = inject(ReflectionHelper.class);
+	private final HandlerFactory handlerFactory = inject(HandlerFactory.class);
 
 	private HttpServer serverInstance;
 
 	public void startServer() throws IOException {
+		createInstance().subscribe(instance -> {
+			this.serverInstance = instance;
+			reflectionHelper.getContextHandlers().stream() //
+					.map(serverConfigHelper::addWebController) //
+					.forEach(observable -> observable.subscribe(obj -> {
+						setHandler(obj);
+						MVCHandler.getWebControllers().add(obj);
+					}));
+			serverInstance.start();
+		});
+	}
+
+	private Observable<HttpServer> createInstance() throws IOException {
 		try {
 			logger().info("Starting server on port " + config.get(PORT));
-			serverInstance = HttpServer.create(new InetSocketAddress(config.getInt(PORT)), 0);
+			return Observable.just(HttpServer.create(new InetSocketAddress(config.getInt(PORT)), 0));
 		} catch (IOException e) {
 			logger().error("Cannot start server...", e);
 			throw e;
 		}
-		reflectionHelper.getContextHandlers() //
-				.forEach(claz -> serverConfigHelper.addWebController(this, claz));
-		serverInstance.start();
+	}
+
+	public void setHandler(IController ctrl) {
+		String contextPath = reflectionHelper.getContextPath(ctrl.getClass());
+		String handler = config.get("contextHandler." + contextPath, WebHandler.class.getSimpleName());
+		HttpContext ctx = registerHandler(contextPath, handlerFactory.get(handler));
+		setHandlerController(ctx, ctrl);
 	}
 
 	public HttpContext registerHandler(String contextPath, HttpHandler handler) {
--- src/main/java/com/schibsted/webapp/server/helper/ServerConfigHelper.java
+++ src/main/java/com/schibsted/webapp/server/helper/ServerConfigHelper.java
@@ -9,46 +9,38 @@ import javax.inject.Singleton;
 import com.schibsted.webapp.server.Config;
 import com.schibsted.webapp.server.IController;
 import com.schibsted.webapp.server.ILogger;
-import com.schibsted.webapp.server.Server;
 import com.schibsted.webapp.server.annotation.Authenticated;
 import com.schibsted.webapp.server.filter.AuthFilter;
 import com.schibsted.webapp.server.filter.ParamsFilter;
-import com.schibsted.webapp.server.handler.HandlerFactory;
-import com.schibsted.webapp.server.handler.MVCHandler;
-import com.schibsted.webapp.server.handler.WebHandler;
 import com.sun.net.httpserver.Filter;
 import com.sun.net.httpserver.HttpContext;
+
+import rx.Observable;
 
 @Singleton
 @SuppressWarnings("restriction")
 public class ServerConfigHelper implements ILogger {
 
-	
-	private final Config config = inject(Config.class);
-
-	private final HandlerFactory handlerFactory = inject(HandlerFactory.class);
 	private final ReflectionHelper reflectionHelper = inject(ReflectionHelper.class);
 	private final ParamsFilter paramsFilter = inject(ParamsFilter.class);
 	private final AuthFilter authFilter = inject(AuthFilter.class);
-	
-	public void addWebController(Server server, Class<?> claz) {
-		try {
-			if (!reflectionHelper.isControllerCandidate(claz))
-				return;
-			logger().debug("Adding controller: {}", claz.getName());
-			IController obj = (IController) claz.newInstance();
-			setWebHandlers(server,obj);
-			MVCHandler.getWebControllers().add(obj);
-		} catch (InstantiationException | IllegalAccessException e) {
-			logger().error("", e);
-		}
+
+	public Observable<IController> addWebController(Class<?> claz) {
+		return createControllerInstance(claz);
 	}
 
-	public void setWebHandlers(Server server, IController ctrl) {
-		String contextPath = reflectionHelper.getContextPath(ctrl.getClass());
-		String handler = config.get("contextHandler." + contextPath, WebHandler.class.getSimpleName());
-		HttpContext ctx = server.registerHandler(contextPath, handlerFactory.get(handler));
-		setHandlerController(ctx, ctrl);
+	private Observable<IController> createControllerInstance(Class<?> claz) {
+		Observable<IController> res = Observable.empty();
+		if (reflectionHelper.isControllerCandidate(claz)) {
+			logger().debug("Adding controller: {}", claz.getName());
+			try {
+				res = Observable.just((IController) claz.newInstance()).share();
+			} catch (InstantiationException | IllegalAccessException e) {
+				logger().error("", e);
+				
+			}
+		}
+		return res;
 	}
 
 	public void setHandlerFilters(HttpContext ctx, IController ctrl) {
@@ -57,7 +49,7 @@ public class ServerConfigHelper implements ILogger {
 			filters.add(authFilter);
 		filters.add(paramsFilter);
 	}
-	
+
 	public void setHandlerController(HttpContext ctx, IController ctrl) {
 		ctx.getAttributes().put(Config.CONTROLLER, ctrl);
 		setHandlerFilters(ctx, ctrl);
